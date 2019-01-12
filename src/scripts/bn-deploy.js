04c27dace429664b255e6a9e4383904741f1c47e
willurd <william.bowers@gmail.com>
2019-01-11 19:05:33 -0800

    Wrote a deploy script for updating the game with new scripts from the repo

---
+++ src/scripts/bn-deploy.js
@@ -0,0 +1,41 @@
+const SCRIPTS = [
+  'bn-boot-1.js',
+  'bn-boot-2.js',
+  'bn-boot-3.js',
+  'bn-boot-4.js',
+  'bn-boot-5.js',
+  'bn-boot.js',
+  'bn-getmoney.js',
+  'bn-hack.js',
+  'bn-own.js',
+  'bn-report.js',
+  'bn-update-owned.js',
+  'bn-utils.js',
+  'bn-weaken.js',
+  'constants.js',
+  'lib-ds.js',
+  'lib-hosts.js',
+];
+
+const joinPath = (...parts) => {
+  return parts.join('/').replace('//', '/');
+};
+
+export async function main(ns) {
+  const scriptServer = ns.args[0] || 'http://localhost:8080';
+  const scriptRoot = 'scripts';
+
+  for (const script of SCRIPTS) {
+    const url = joinPath(scriptServer, scriptRoot, script);
+
+    ns.tprint(`Downloading ${script}`);
+    const success = await ns.wget(url, script);
+
+    if (!success) {
+      ns.tprint(`Unable to download script from url: ${url}`);
+      return;
+    }
+  }
+
+  ns.tprint('Botnet deployed');
+}
